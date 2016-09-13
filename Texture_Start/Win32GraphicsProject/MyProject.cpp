c4333a7f22587e06699cb9e90ba75c6de68fcf0b
Paul Boyko <humbertosanchas@hotmail.com>
2016-09-13 09:58:51 -0400

    stuff

---
+++ Texture_Start/Win32GraphicsProject/MyProject.cpp
@@ -0,0 +1,118 @@
+#include "MyProject.h"
+#include <Windowsx.h> // for GET__LPARAM macros
+#include <WICTextureLoader.h>
+
+//----------------------------------------------------------------------------------------------
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nShowCmd)
+{
+	MyProject application(hInstance);    // Create the class variable
+
+	if (application.InitWindowsApp(L"Font Example", nShowCmd) == false)    // Initialize the window, if all is well show and update it so it displays
+	{
+		return 0;                   // Error creating the window, terminate application
+	}
+
+	if (application.InitializeDirect3D())
+	{
+		application.SetDepthStencil(true);      // Tell DirectX class to create and maintain a depth stencil buffer
+		application.InitializeFonts();
+		application.InitializeTextures();
+		application.MessageLoop();				// Window has been successfully created, start the application message loop
+	}
+
+	return 0;
+}
+
+//----------------------------------------------------------------------------------------------
+// Constructor
+//----------------------------------------------------------------------------------------------
+MyProject::MyProject(HINSTANCE hInstance)
+	: DirectXClass(hInstance)
+{
+	message = L"Mouse was clicked here";
+	mouseX = -1000;
+	mouseY = -1000;
+
+	pTexture = NULL;
+}
+
+//----------------------------------------------------------------------------------------------
+// Destructor
+//----------------------------------------------------------------------------------------------
+MyProject::~MyProject()
+{
+	SAFE_RELEASE(pTexture);
+}
+
+//----------------------------------------------------------------------------------------------
+// Initialize any fonts we need to use here
+//----------------------------------------------------------------------------------------------
+void MyProject::InitializeFonts()
+{
+	timesNewRoman24Bold.InitializeFont(D3DDevice, DeviceContext, L"..\\Font\\TimesnewRoman24Bold.spritefont");
+}
+
+//---------------------------------------------------------------------------------------------
+// Initialize any textures we need to use here
+//---------------------------------------------------------------------------------------------
+void MyProject::InitializeTextures()
+{
+	HRESULT result = DirectX::CreateWICTextureFromFile(D3DDevice, L"..\\Textures\\Flower.PNG", (ID3D11Resource**)&pTexture, NULL);
+	if (result != S_OK)
+	{
+		MessageBox(mainWnd, L"Could not load texture", L"ERROR", MB_OK | MB_ICONERROR);
+	}
+}
+
+//----------------------------------------------------------------------------------------------
+// Window message handler
+//----------------------------------------------------------------------------------------------
+LRESULT MyProject::ProcessWindowMessages(UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_LBUTTONUP:
+		mouseX = GET_X_LPARAM(lParam);
+		mouseY = GET_Y_LPARAM(lParam);
+		return 0;
+
+	}
+
+	// let the base class handle remaining messages
+	return DirectXClass::ProcessWindowMessages(msg, wParam, lParam);
+}
+
+//----------------------------------------------------------------------------------------------
+// Called by the render loop to render a single frame
+//----------------------------------------------------------------------------------------------
+void MyProject::Render(void)
+{
+
+	timesNewRoman24Bold.PrintMessage(mouseX, mouseY, message, FC_RED);
+
+	if (pTexture != NULL)
+	{
+		//Get the descriptior of the texture
+		D3D11_TEXTURE2D_DESC desc;
+		pTexture->GetDesc(&desc);
+
+		//describe the region we are copying from the source
+		D3D11_BOX sourceRegion;
+		sourceRegion.left = 0;
+		sourceRegion.right = desc.Width;
+		sourceRegion.top = 0;
+		sourceRegion.bottom = desc.Height;
+		sourceRegion.front = 0;
+		sourceRegion.back = 1;
+
+
+
+		//Copy the texture to the backbuffer (render to the backbuffer)
+		DeviceContext->CopySubresourceRegion(BackBuffer, 0, 100, 100, 0, pTexture, 0, &sourceRegion);
+	}
+
+
+	
+	// render the base class
+	DirectXClass::Render();
+}
---
+++ Texture_Start/Win32GraphicsProject/MyProject.h
@@ -0,0 +1,50 @@
+#ifndef _MyProject_h
+#define _MyProject_h
+
+#include "DirectX.h"
+#include "Font.h"
+
+//----------------------------------------------------------------------------------------------
+// Main project class
+//	Inherits the directx class to help us initalize directX
+//----------------------------------------------------------------------------------------------
+
+class MyProject : public DirectXClass
+{
+public:
+	// constructor
+	MyProject(HINSTANCE hInstance);
+	~MyProject();
+
+	// Initialize any fonts we need to use
+	void InitializeFonts();
+
+	//Initialize textures we want to use
+	void InitializeTextures();
+
+
+
+	// window message handler
+	LRESULT ProcessWindowMessages(UINT msg, WPARAM wParam, LPARAM lParam);
+
+	// Called by the render loop to render a single frame
+	void Render(void);
+
+private:
+
+	//FONT Members
+	FontType timesNewRoman24Bold;
+	
+	int mouseX;
+	int mouseY;
+
+	std::wstring message;
+
+
+	//Texture members
+	//create a pointer to a texture
+	ID3D11Texture2D *pTexture;
+	
+};
+
+#endif
